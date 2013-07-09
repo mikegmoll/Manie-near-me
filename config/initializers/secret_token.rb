1bcea5830e0a7a1a6071d76ed8b4465e07bf1203
mikegmoll <mike.g.moll@gmail.com>
2013-07-09 05:21:15 -0700

    sorted .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MNM::Application.config.secret_token = 'b1c5ed6f528168ec17710ca0dd88180231335d7992e7768a38a61e80c59f2ce8cf2c32cd200a612a0564336e544657044f92f2bf787bc7f11e6f872c85f1e8e4'
